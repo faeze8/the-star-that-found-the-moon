b8842bce657a2c4815ad2f350c0d9530f323f9c0
faeze8 <faezeh.mayne25@gmail.com>
2026-07-23 21:39:17 +0330

    fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/the-star-that-found-the-moon"> 
     <App />
   </BrowserRouter>
 );
